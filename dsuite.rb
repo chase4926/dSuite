eabedaed991b3fa7d70bf2a9246944f4366d4a26
Chase Arnold <chase4926@gmail.com>
2011-12-04 04:57:21 -0800

    Added Elevator and Screen plugins

--- dsuite.rb
+++ dsuite.rb
@@ -1,5 +1,5 @@
 class DSuitePlugin
-  include Purugin::Plugin
+  include Purugin::Plugin, Purugin::Colors
   description('dSuite', 0.3)
   
   def decode(value)
@@ -32,6 +32,50 @@ class DSuitePlugin
   
   def locations_path()
     @path ||= File.join getDataFolder, 'locations.data'
+  end
+  
+  def change_sign(sign_object, text_array)
+    #server.broadcast_message(convert_array_to_readable_string(sign_object.methods))
+    sign_object.set_line(0, text_array[0])
+    sign_object.set_line(1, text_array[1])
+    sign_object.set_line(2, text_array[2])
+    sign_object.set_line(3, text_array[3])
+    # Now perform a block update to change the sign
+    sign_object.update
+  end
+  
+  def parse_sign(sign_state)
+    return [sign_state.get_line(0),sign_state.get_line(1),sign_state.get_line(2),sign_state.get_line(3)]
+  end
+  
+  def check_direction_for_screen(direction, start_block)
+    second_block = start_block.block_at(direction)
+    if second_block.is? :iron_block
+      # Iron blocks are there, now see if signs are
+      sign1 = start_block.block_at(:up)
+      sign2 = second_block.block_at(:up)
+      screen_sign = sign2.block_at(direction,2)
+      if sign1.is? :sign_post and sign2.is? :sign_post and screen_sign.is? :wall_sign then
+        return [parse_sign(sign1.state),parse_sign(sign2.state),screen_sign.state]
+      end
+    end
+  end
+  
+  def screen_setup(first_block)
+    #North
+    north = check_direction_for_screen(:north, first_block)
+    return north if north != nil
+    #South
+    south = check_direction_for_screen(:south, first_block)
+    return south if south != nil
+    #East
+    east = check_direction_for_screen(:east, first_block)
+    return east if east != nil
+    #West
+    west = check_direction_for_screen(:west, first_block)
+    return west if west != nil
+    #---
+    return nil
   end
   
   def check_for_blocks(original_block, direction, max_check)
@@ -109,11 +153,29 @@ class DSuitePlugin
       end
     end
     
+    event (:redstone_change) do |e|
+      #server.broadcast_message(e.get_new_current.inspect)
+      block = e.get_block
+      if block.is? :sign_post and block.block_at(:down).is? :iron_block then
+        signs = screen_setup(block.block_at(:down))
+        if signs != nil then
+          sign1_array = signs[0]
+          sign2_array = signs[1]
+          screen_sign = signs[2]
+          if e.get_new_current == 0 then # Unpowered
+            change_sign(screen_sign, sign1_array)
+          else # Powered
+            change_sign(screen_sign, sign2_array)
+          end
+        end
+      end
+    end
+    
     event(:player_interact) do |e|
       player = e.player
       block = player.world.block_at(player.location).block_at(:down)
       clicked_block = e.clicked_block
-      if block.is? :gold_block and  clicked_block.is?(:stone_button) then
+      if block.is? :gold_block and clicked_block.is?(:stone_button) then
         # Standing on a gold block, clicking on a stone_button
         button_level = (clicked_block.location.to_a[1] - block.location.to_a[1]).to_i
         if button_level == 1 then
